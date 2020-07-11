7dc1d6d262b550580e1c2a9f6012fdf51d9ca24c
ramsbeno <benoit.rams@etu.utc.fr>
2020-07-11 18:41:47 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./App.css";
+import Dice from "./Dice";
+
+function App() {
+    const [num, setNum] = useState(0);
+    const changeNumber = () => {
+        setNum(Math.floor(Math.random() * 6) + 1);
+    };
+    return (
+        <div className="App">
+            <div className="header">
+                <h1>BE DICE</h1>
+            </div>
+            <div className="main">
+                <button onClick={changeNumber}>Roll a dice</button>
+                <Dice number={num}></Dice>
+            </div>
+        </div>
+    );
+}
+
+export default App;
