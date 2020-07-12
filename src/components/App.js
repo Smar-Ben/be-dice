5191b71da2e181c7996381c6d6449f6d612a09b6
ramsbeno <benoit.rams@etu.utc.fr>
2020-07-12 22:50:26 +0200

    possibilité d'ajouter plusieurs dés 6 faces

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,59 @@ import "./App.css";
 import Dice from "./Dice";
 
 function App() {
-    const [num, setNum] = useState(0);
+    const [numDice, setDice] = useState(1);
+    const [numFace, setFace] = useState(6);
+    const [num, setNum] = useState([]);
 
     const changeNumber = () => {
-        setNum(Math.floor(Math.random() * 6) + 1);
+        const newNum = new Array(numDice);
+        for (let i = 0; i < numDice; i++) {
+            newNum[i] = Math.floor(Math.random() * numFace) + 1;
+        }
+        setNum(newNum);
     };
-
+    const changeNumberDice = (e) => {
+        setDice(e.target.value);
+    };
+    const changeFaceNum = (e) => {
+        setFace(e.target.value);
+    };
     return (
         <div className="App">
             <div className="header">
                 <h1>BE DICE</h1>
             </div>
             <div className="main">
+                <div className="selector">
+                    <p>
+                        Roll{" "}
+                        <input
+                            type="number"
+                            id="dices"
+                            name="dices"
+                            min="1"
+                            max="200"
+                            value={numDice}
+                            onChange={changeNumberDice}
+                        ></input>{" "}
+                        dices with{" "}
+                        <select name="faces" id="faces" value={numFace} onChange={changeFaceNum}>
+                            <option value={4}>4</option>
+                            <option value={6}>6</option>
+                            <option value={8}>8</option>
+                            <option value={10}>10</option>
+                            <option value={12}>12</option>
+                            <option value={20}>20</option>
+                        </select>{" "}
+                        faces
+                    </p>
+                </div>
                 <button onClick={changeNumber}>Roll a dice</button>
-                {num !== 0 && <Dice number={num}></Dice>}
+                <div className="result">
+                    {num.map((el, index) => (
+                        <Dice key={index} number={el}></Dice>
+                    ))}
+                </div>
             </div>
         </div>
     );
